f2b34a80d560b317742a433fa678d1e66f69336e
杨佳乐 <jlyang1999@163.com>
2020-02-25 19:21:02 +0800

    框架搭建

---
+++ main.js
@@ -0,0 +1,28 @@
+// 通过 require electron 来控制程序
+const { app, BrowserWindow } = require('electron')
+
+// app加载完毕
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 加载 html 页面
+  mainWindow.loadURL('http://localhost:4200/')
+
+  // 打开控制台
+  mainWindow.webContents.openDevTools()
+})
+
+// macOS 的一些设置
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
